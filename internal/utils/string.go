1ee5d1361aa3fee6083d1def290e78822d7699b0
Vsevolod Girenko <vs.girenko@mail.ru>
2024-12-23 13:20:10 +0300

    chore: global type alias for [20]byte slices

---
+++ internal/utils/string.go
@@ -0,0 +1,6 @@
+package utils
+
+// An alias for `utils.BTString` type used all over as default
+// length of BitTorrent information strings like `info_hash`
+// or `peer_id`
+type BTString [20]byte
