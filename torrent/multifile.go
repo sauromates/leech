2e7d909f06f9f1db3a98f96cb1d5630624de1283
Vsevolod Girenko <vs.girenko@mail.ru>
2024-12-23 13:21:06 +0300

    feat: split single and multiple files torrents implementations

---
+++ torrent/multifile.go
@@ -0,0 +1,127 @@
+package torrent
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/sauromates/leech/internal/peers"
+	"github.com/sauromates/leech/internal/utils"
+	"github.com/sauromates/leech/worker"
+)
+
+type MultiFileTorrent struct {
+	BaseTorrent
+	Paths []utils.FileInfo
+}
+
+func (torrent MultiFileTorrent) TotalSizeBytes() int {
+	size := 0
+	for _, file := range torrent.Paths {
+		size += file.Length
+	}
+
+	return size
+}
+
+func (torrent *MultiFileTorrent) Download(path string) ([]byte, error) {
+	queue := make(chan *worker.TaskItem, len(torrent.PieceHashes))
+	results := make(chan *worker.TaskResult)
+	pool := make(chan *peers.Peer, len(torrent.Peers))
+
+	for i, hash := range torrent.PieceHashes {
+		queue <- &worker.TaskItem{Index: i, Hash: hash, Length: torrent.PieceSize(i)}
+	}
+
+	for _, peer := range torrent.Peers {
+		pool <- &peer
+	}
+
+	done := 0
+	for done < len(torrent.PieceHashes) {
+		select {
+		case piece := <-results:
+			if err := torrent.Write(path, piece); err != nil {
+				return nil, err
+			}
+
+			log.Printf("Downloaded piece %d, %d left", piece.Index, len(torrent.PieceHashes))
+			done++
+		case peer := <-pool:
+			go torrent.startWorker(peer, queue, results, pool)
+			numWorkers := runtime.NumGoroutine() - 1
+			log.Printf("Started worker for peer %s, currently %d are running", peer.String(), numWorkers)
+		}
+	}
+
+	close(queue)
+	close(results)
+	close(pool)
+
+	return []byte{}, nil
+}
+
+func (torrent *MultiFileTorrent) Write(basePath string, piece *worker.TaskResult) error {
+	filename, err := torrent.WhichFile(piece.Index)
+	if err != nil {
+		return err
+	}
+
+	filepath := filepath.Join(basePath, filename)
+	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	if _, err := file.Write(piece.Content); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// WhichFile determines which file a piece belongs to based on its index
+func (torrent *MultiFileTorrent) WhichFile(index int) (string, error) {
+	type bound struct {
+		name  string
+		begin int
+		end   int
+	}
+
+	// Assemble a slice of file boundaries
+	bounds := make([]bound, len(torrent.Paths))
+	for i, file := range torrent.Paths {
+		var begin int
+		if i == 0 {
+			begin = 0
+		} else {
+			begin = bounds[i-1].end + 1
+		}
+
+		filename := filepath.Join(file.Path...)
+
+		bounds[i] = bound{filename, begin, begin + file.Length}
+	}
+
+	pieceBegin, _ := torrent.PieceBounds(index)
+	// Quickly search for the right interval for received piece index
+	left, right := 0, len(bounds)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		file := bounds[mid]
+
+		if pieceBegin >= file.begin && pieceBegin <= file.end {
+			return file.name, nil
+		} else if pieceBegin < file.begin {
+			right = mid - 1
+		} else {
+			right = mid + 1
+		}
+	}
+
+	return "", fmt.Errorf("failed to associate piece %d with a file", index)
+}
---
+++ torrent/singlefile.go
@@ -0,0 +1,52 @@
+package torrent
+
+import (
+	"log"
+	"runtime"
+
+	"github.com/sauromates/leech/internal/peers"
+	"github.com/sauromates/leech/worker"
+)
+
+type SingleFileTorrent struct {
+	BaseTorrent
+}
+
+func (torrent SingleFileTorrent) TotalSizeBytes() int {
+	return torrent.Length
+}
+
+func (torrent *SingleFileTorrent) Download(path string) ([]byte, error) {
+	queue := make(chan *worker.TaskItem, len(torrent.PieceHashes))
+	results := make(chan *worker.TaskResult)
+	pool := make(chan *peers.Peer, len(torrent.Peers))
+
+	for i, hash := range torrent.PieceHashes {
+		queue <- &worker.TaskItem{Index: i, Hash: hash, Length: torrent.PieceSize(i)}
+	}
+
+	for _, peer := range torrent.Peers {
+		pool <- &peer
+	}
+
+	content, done := make([]byte, torrent.Length), 0
+	for done < len(torrent.PieceHashes) {
+		select {
+		case piece := <-results:
+			begin, end := torrent.PieceBounds(piece.Index)
+			copy(content[begin:end], piece.Content)
+
+			done++
+		case peer := <-pool:
+			go torrent.startWorker(peer, queue, results, pool)
+			numWorkers := runtime.NumGoroutine() - 1
+			log.Printf("Started worker for peer %s, currently %d are running", peer.String(), numWorkers)
+		}
+	}
+
+	close(queue)
+	close(results)
+	close(pool)
+
+	return content, nil
+}
