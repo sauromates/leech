95fc9bb066297f6838883954b1c80b8e1dd0c5ba
Vsevolod Girenko <vs.girenko@mail.ru>
2025-01-05 11:20:42 +0300

    feat: calculate absolute files bounds on decoding step

--- internal/utils/files.go
+++ internal/utils/files.go
@@ -1,6 +1,12 @@
 package utils
 
-type FileInfo struct {
-	Length int      `bencode:"length"`
-	Path   []string `bencode:"path"`
+// PathInfo is processed FileInfo with calculated absolute path, offset
+// within the torrent and relative length
+type PathInfo struct {
+	// Path is a concatenated path from bencoded FileInfo
+	Path string
+	// Offset is starting point relative to the whole torrent
+	Offset int
+	// Length is an end position within the whole torrent
+	Length int
 }
