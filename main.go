7fdf64ebe810e2135ce5f908d746fe28b3682d55
Vsevolod Girenko <vs.girenko@mail.ru>
2025-01-09 10:03:39 +0300

    feat: separate peers management from download process

--- main.go
+++ main.go
@@ -5,38 +5,83 @@ import (
 	"log"
 	"os"
 
+	"github.com/sauromates/leech/dht"
+	"github.com/sauromates/leech/internal/bthash"
+	"github.com/sauromates/leech/internal/metadata"
+	"github.com/sauromates/leech/internal/peers"
+	"github.com/sauromates/leech/magnetlink"
 	"github.com/sauromates/leech/torrent"
-	"github.com/sauromates/leech/torrentfile"
 )
 
-func main() {
+var (
+	clientID bthash.Hash = bthash.NewRandom()
+	port     uint16      = 6881
+	pool     chan *peers.Peer
+)
+
+func init() {
 	if err := configureLogs("leech.log"); err != nil {
-		log.Fatal(err)
+		exit(err)
 	}
 
-	inputPath := os.Args[1]
-	torrentfile, err := torrentfile.Open(inputPath)
+	pool = make(chan *peers.Peer)
+}
+
+func main() {
+	source := os.Args[1]
+
+	meta, err := getMetadata(source)
 	if err != nil {
-		log.Fatal(err)
+		exit(err)
 	}
 
-	dir, err := createDownloadDir(torrentfile.Name)
+	dir, err := createDownloadDir(meta.Info.Name)
 	if err != nil {
-		log.Fatal(err)
+		exit(err)
 	}
 
-	torrent, err := torrent.CreateFromTorrentFile(torrentfile)
-	if err != nil {
-		log.Fatal(err)
+	torrent := torrent.Torrent{
+		Meta:         meta,
+		ClientID:     clientID,
+		ClientPort:   port,
+		Peers:        pool,
+		DownloadPath: dir,
 	}
 
 	fmt.Printf("Downloading\n---\n%s\n", torrent)
 
-	if err := torrent.Download(dir); err != nil {
-		log.Fatal(err)
+	// Spin up peer discovery in a separate goroutine
+	go torrent.FindPeers()
+
+	if err := torrent.Download(); err != nil {
+		exit(err)
 	}
 
 	printResultDetails(dir)
+}
+
+// getMetadata issues a search for torrent metadata based on input's type:
+// magnet link input will lead to search in DHT for a peer holding the info
+// while `.torrent` file inputs are decoded via [torrentfile] package.
+func getMetadata(src string) (*metadata.Metadata, error) {
+	if magnetlink.Check(src) {
+		return dht.FindMetadata()
+	}
+
+	file, err := os.Open(src)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	return metadata.Parse(file)
+}
+
+// exit prints error and returns non-zero code
+func exit(err error) {
+	fmt.Println(err)
+	os.Exit(1)
 }
 
 // configureLogs sets default log output to a file with given path
